d99b9029771762a0df50b0e8dbb55e3fb25196ed
Brendan Burkhart <blackravenbrendan@gmail.com>
2018-09-29 17:16:54 -0700

    Add api tests for new endpoints

--- api-tests/api.test.js
+++ api-tests/api.test.js
@@ -29,6 +29,18 @@ expect.extend({
     }
     return { pass: true }
   },
+  toBeATeamKey(received) {
+    try {
+      expect(received.length).toBeGreaterThan(3)
+      expect(received.slice(0, 3)).toEqual('frc')
+    } catch (error) {
+      return {
+        message: () => `expected ${received} to be a team key`,
+        pass: false,
+      }
+    }
+    return { pass: true }
+  },
   toBeUndefinedOr(received, type) {
     if (received === undefined) {
       return { pass: true }
@@ -60,6 +72,8 @@ const config = jsyaml.safeLoad(
 
 const addr = `http://${config.server.address}/`
 
+const youtubeOrTwitchRegex = /^(youtube|twitch)$/gm
+
 test('the api is alive', () => {
   return fetch(addr)
 })
@@ -70,8 +84,8 @@ test('/events endpoint', async () => {
   expect(d.data.length).toBeGreaterThan(1)
   d.data.forEach(event => {
     expect(event.name).toBeA(String)
-    expect(event.startDate).toBeA(String)
-    expect(event.endDate).toBeA(String)
+    expect(event.startDate).toBeADateString()
+    expect(event.endDate).toBeADateString()
     expect(event.location).toBeA(Object)
     expect(event.location.lat).toBeA(Number)
     expect(event.location.lon).toBeA(Number)
@@ -88,4 +102,94 @@ test('/events endpoint', async () => {
       'district',
     ])
   })
+})
+
+test('/events/{eventKey}/info endpoint', async () => {
+  const d = await fetch(addr + '/events/2018flor/info').then(d => d.json())
+  const info = d.data
+  expect(info.key).toEqual('2018flor')
+  expect(info.name).toBeA(String)
+  expect(info.startDate).toBeADateString()
+  expect(info.endDate).toBeADateString()
+  expect(info.location).toBeA(Object)
+  expect(info.location.name).toBeA(String)
+  expect(info.location.lat).toBeA(Number)
+  expect(info.location.lon).toBeA(Number)
+  expect(info.key).toBeA(String)
+  expect(info.district).toBeUndefinedOr(String)
+  expect(info.week).toBeUndefinedOr(Number)
+  expect(info.webcasts).toEqual(expect.any(Array))
+  info.webcasts.forEach(webcast => {
+    expect(webcast.url).toBeA(String)
+    expect(webcast.type).toBeA(String)
+    expect(webcast.type).toEqual(expect.stringMatching(youtubeOrTwitchRegex))
+    expect(Object.keys(webcast)).toBeASubsetOf(['type', 'url'])
+  })
+  expect(Object.keys(info)).toBeASubsetOf([
+    'key',
+    'name',
+    'week',
+    'startDate',
+    'endDate',
+    'location',
+    'district',
+    'webcasts',
+  ])
+})
+
+test('/events/{eventKey}/matches endpoint', async () => {
+  const d = await fetch(addr + '/events/2018flor/matches').then(d => d.json())
+  expect(d).toEqual({ data: expect.any(Array) })
+  expect(d.data.length).toBeGreaterThan(1)
+  d.data.forEach(match => {
+    expect(match.key).toBeA(String)
+    expect(match.time).toBeADateString()
+    expect(match.redScore).toBeUndefinedOr(Number)
+    expect(match.blueScore).toBeUndefinedOr(Number)
+    expect(match.redAlliance).toEqual(expect.any(Array))
+    match.redAlliance.forEach(team => {
+      expect(team).toBeATeamKey()
+    })
+    expect(match.blueAlliance).toEqual(expect.any(Array))
+    match.blueAlliance.forEach(team => {
+      expect(team).toBeATeamKey()
+    })
+    expect(Object.keys(match)).toBeASubsetOf([
+      'key',
+      'time',
+      'redAlliance',
+      'blueAlliance',
+      'redScore',
+      'blueScore',
+    ])
+  })
+})
+
+test('/events/{eventKey}/matches/{matchKey}/info endpoint', async () => {
+  const d = await fetch(
+    addr + '/events/2018flor/matches/2018flor_qm28/info',
+  ).then(d => d.json())
+  const info = d.data
+  expect(info.key).toEqual('2018flor_qm28')
+  expect(info.time).toBeADateString()
+  expect(info.redScore).toBeUndefinedOr(Number)
+  expect(info.blueScore).toBeUndefinedOr(Number)
+  expect(info.redAlliance).toEqual(expect.any(Array))
+  expect(info.redAlliance.length).toEqual(3)
+  info.redAlliance.forEach(team => {
+    expect(team).toBeATeamKey()
+  })
+  expect(info.blueAlliance).toEqual(expect.any(Array))
+  expect(info.blueAlliance.length).toEqual(3)
+  info.blueAlliance.forEach(team => {
+    expect(team).toBeATeamKey()
+  })
+  expect(Object.keys(info)).toBeASubsetOf([
+    'key',
+    'time',
+    'redAlliance',
+    'blueAlliance',
+    'redScore',
+    'blueScore',
+  ])
 })
