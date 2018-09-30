f132b22626b3b27bd143e5a8325c8de1d38a7bdc
Franklin Harding <franklinharding0.0@gmail.com>
2018-09-29 21:24:28 -0700

    Write exit code if we got an error

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/Pigmice2733/peregrine-backend/internal/config"
@@ -11,46 +12,50 @@ import (
 	"github.com/golang-migrate/migrate/database/postgres"
 	_ "github.com/golang-migrate/migrate/source/file"
 	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
 )
 
 func main() {
 	var steps = flag.Int("steps", 0, "Number of steps to migrate. Leave unspecified to migrate all the way up or down.")
 	var up = flag.Bool("up", false, "Migrate up. Cannot be used with -down.")
 	var down = flag.Bool("down", false, "Migrate down. Cannot be used with -up.")
-	var migrationstable = flag.String("migrationstable", "migrations", "Name of SQL table to store migrations in.")
+	var migrationsTable = flag.String("migrationsTable", "migrations", "Name of SQL table to store migrations in.")
 	var basePath = flag.String("basePath", "", "Path to the etc directory where the config file is.")
 
 	flag.Parse()
 
+	if err := run(*steps, *up, *down, *migrationsTable, *basePath); err != nil {
+		fmt.Printf("got error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(steps int, up, down bool, migrationsTable, basePath string) error {
 	// Neither are set or both are set
-	if *up == *down {
-		fmt.Printf("Error: must specify either -up or -down\n")
-		return
+	if up == down {
+		return fmt.Errorf("must specify either -up or -down")
 	}
 
-	c, err := config.Open(*basePath)
+	c, err := config.Open(basePath)
 	if err != nil {
-		fmt.Printf("Error: opening config: %v\n", err)
-		return
+		return errors.Wrap(err, "opening config")
 	}
 
 	db, err := sql.Open("postgres", c.Database.ConnectionInfo())
 	if err != nil {
-		fmt.Printf("Error: connecting to db: %v\n", err)
-		return
+		return errors.Wrap(err, "connecting to database")
 	}
 	defer db.Close()
 
-	config := &postgres.Config{MigrationsTable: *migrationstable, DatabaseName: c.Database.Name}
+	config := &postgres.Config{MigrationsTable: migrationsTable, DatabaseName: c.Database.Name}
 	driver, err := postgres.WithInstance(db, config)
 	if err != nil {
-		fmt.Printf("Error: getting PostgreSQL driver: %v\n", err)
-		return
+		return errors.Wrap(err, "getting postgresql driver")
 	}
 
 	migrationsSource := "file://"
-	if *basePath != "" {
-		migrationsSource += filepath.ToSlash(filepath.Clean(*basePath)) + "/"
+	if basePath != "" {
+		migrationsSource += filepath.ToSlash(filepath.Clean(basePath)) + "/"
 	} else {
 		migrationsSource += "./"
 	}
@@ -60,39 +65,33 @@ func main() {
 		migrationsSource,
 		c.Database.Name, driver)
 	if err != nil {
-		fmt.Printf("Error: creating migrations: %v\n", err)
-		return
+		return errors.Wrap(err, "opening migrations")
 	}
 
 	defer m.Close()
 
-	if *steps == 0 && *up {
+	if steps == 0 && up {
 		err = m.Up()
-	} else if *steps == 0 && *down {
+	} else if steps == 0 && down {
 		err = m.Down()
 	} else {
-		if *down {
-			*steps = -*steps
+		if down {
+			steps = -steps
 		}
-		err = m.Steps(*steps)
+		err = m.Steps(steps)
 	}
 
-	if err != nil {
-		fmt.Printf("Error: running migrations: %v\n", err)
-		return
+	if err != nil && err != migrate.ErrNoChange {
+		return errors.Wrap(err, "running migrations")
 	}
-
-	fmt.Println("Migrations successfully run")
 
 	srcErr, dbErr := m.Close()
 	if srcErr != nil {
-		fmt.Printf("Error: closing migrations source: %v\n", srcErr)
+		return errors.Wrap(srcErr, "closing migrations source")
 	}
 	if dbErr != nil {
-		fmt.Printf("Error: closing connection to database: %v\n", dbErr)
+		return errors.Wrap(dbErr, "closing connection to database")
 	}
 
-	if err = db.Close(); err != nil {
-		fmt.Printf("Error: closing database: %v\n", err)
-	}
+	return nil
 }
