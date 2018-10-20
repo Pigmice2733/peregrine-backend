f7c92a97d629f554fcc621532e26207755d24256
Franklin Harding <franklinharding0.0@gmail.com>
2018-10-19 23:28:27 -0700

    Use sqlx everywhere it makes sense

--- internal/store/alliances.go
+++ internal/store/alliances.go
@@ -3,6 +3,7 @@ package store
 import (
 	"database/sql"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 )
 
@@ -21,7 +22,7 @@ func (s *Service) GetMatchAlliance(matchKey string, getBlue bool) ([]string, err
 
 // AlliancesUpsert upserts the red and blue alliances for a specific match.
 // matchKey is the key of the match. Upsert done within transaction.
-func (s *Service) AlliancesUpsert(matchKey string, blueAlliance []string, redAlliance []string, tx *sql.Tx) error {
+func (s *Service) AlliancesUpsert(matchKey string, blueAlliance []string, redAlliance []string, tx *sqlx.Tx) error {
 	stmt, err := tx.Prepare(`
 		INSERT INTO alliances (team_keys, match_key, is_blue)
 		VALUES ($1, $2, $3)
