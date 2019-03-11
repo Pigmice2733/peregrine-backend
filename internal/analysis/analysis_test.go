e42a1f3b67701a2190cd7c4fc565c825fe3350cb
Franklin Harding <franklinharding0.0@gmail.com>
2019-03-11 11:01:24 -0700

    Gofmt with the simplify flag

--- internal/analysis/analysis_test.go
+++ internal/analysis/analysis_test.go
@@ -79,33 +79,33 @@ func TestAnalsyzeReports(t *testing.T) {
 	}
 
 	expected := []TeamStats{
-		TeamStats{
-			Auto: map[string]*stat{"foobar": &stat{
+		{
+			Auto: map[string]*stat{"foobar": {
 				Attempts:  MaxAvg{Max: 1, Avg: 0.5, Total: 1, Matches: 2},
 				Successes: MaxAvg{Max: 0, Avg: 0, Total: 0, Matches: 2},
 				Type:      "boolean",
 			}},
-			Teleop: map[string]*stat{"foobar-teleop": &stat{
+			Teleop: map[string]*stat{"foobar-teleop": {
 				Attempts:  MaxAvg{Max: 5, Avg: 3.5, Total: 7, Matches: 2},
 				Successes: MaxAvg{Max: 3, Avg: 2.5, Total: 5, Matches: 2},
 				Type:      "number",
 			}},
 			Team: "frc2471",
 		},
-		TeamStats{
-			Auto: map[string]*stat{"foobar": &stat{
+		{
+			Auto: map[string]*stat{"foobar": {
 				Attempts:  MaxAvg{Max: 5, Avg: 3, Total: 6, Matches: 2},
 				Successes: MaxAvg{Max: 3, Avg: 2, Total: 4, Matches: 2},
 				Type:      "boolean",
 			}},
-			Teleop: map[string]*stat{"foobar-teleop": &stat{
+			Teleop: map[string]*stat{"foobar-teleop": {
 				Attempts:  MaxAvg{Max: 8, Avg: 4.5, Total: 9, Matches: 2},
 				Successes: MaxAvg{Max: 6, Avg: 3, Total: 6, Matches: 2},
 				Type:      "number",
 			}},
 			Team: "frc2733",
 		},
-		TeamStats{
+		{
 			Auto:   make(map[string]*stat),
 			Teleop: make(map[string]*stat),
 			Team:   "frc4488",
