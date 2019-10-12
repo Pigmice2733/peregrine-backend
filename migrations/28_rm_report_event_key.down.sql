b0b63999f0cb1a9900b0733f085c24e49918d327
Brendan Burkhart <blackravenbrendan@gmail.com>
2019-10-12 09:54:55 -0700

    Repair migration

--- migrations/28_rm_report_event_key.down.sql
+++ migrations/28_rm_report_event_key.down.sql
@@ -1,5 +1,5 @@
 BEGIN;
+ALTER TABLE reports DROP CONSTRAINT reports_event_key_match_key_team_key_reporter_id_key;
 ALTER TABLE reports DROP COLUMN event_key;
-ALTER TABLE reports DROP CONSTRAINT reports_match_key_team_key_reporter_id_key;
 ALTER TABLE reports ADD CONSTRAINT reports_match_key_team_key_reporter_id_key UNIQUE(match_key,team_key,reporter_id);
 COMMIT;
